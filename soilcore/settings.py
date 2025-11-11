71e300802b18ec318ebcdf885ac32315bf9dd461
Yeasin Arafat Nayem <87275236+yanayem@users.noreply.github.com>
2025-11-11 21:40:41 +0600

    Update settings.py

--- soilcore/settings.py
+++ soilcore/settings.py
@@ -23,11 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-284n^dwcj7we)ny7ucj&@=qnz#olyw*g8e+e)(__!g_h46^fu!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-#ALLOWED_HOSTS = ["soil-monitoring.onrender.com"]
-#CSRF_TRUSTED_ORIGINS = ["https://soil-monitoring.onrender.com"]
+#ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["soil-monitoring.onrender.com"]
+CSRF_TRUSTED_ORIGINS = ["https://soil-monitoring.onrender.com"]
 
 ## fo time zone issue
 
